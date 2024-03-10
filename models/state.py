0a7f54fc1a2aa164d0d7d384974b971ec430539a
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-10 15:38:26 -0700

    add state module

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""thi sis the State Modiule"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    All attrbutes should be empty
+    """
+    name = ""
