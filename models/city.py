eaf91407fd1975b3bd960d15996becc5535985a0
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-10 15:35:58 -0700

    place

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this is the City Module"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    The public class attributes should be empty
+    """
+    state_id = ""
+    name = ""
